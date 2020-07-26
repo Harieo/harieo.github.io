102f8785458eb33fc83bc7f5d692b2cdb4916cc5
Harieo <harieo@hotmail.com>
2020-07-26 20:54:34 +0100

    Changed file extensions from .phtml to .php for consistency

--- public/navbar.php
+++ public/navbar.php
@@ -2,8 +2,8 @@
 echo '<div class="navbar">
             <ul>
                 <li id="navbar-logo">Harieo</li>
-                <li class="navbar-button"><a href="index.phtml">Home</a></li>
-                <li class="navbar-button"><a href="about.phtml">About Me</a></li>
+                <li class="navbar-button"><a href="index.php">Home</a></li>
+                <li class="navbar-button"><a href="about.php">About Me</a></li>
                 <li class="navbar-button"><a href="mailto:business@harieo.net" target="_blank">Contact</a></li>
             </ul>
         </div>';
