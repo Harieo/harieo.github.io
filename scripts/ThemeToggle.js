10a374b73a95506555e3c213c9136e16b7c223b2
Harieo <harieo@hotmail.com>
2022-07-30 10:45:49 +0100

    Added script to allow changing of theme

---
+++ scripts/ThemeToggle.js
@@ -0,0 +1,24 @@
+function detectColorScheme() {
+    let theme = "light";    //default to light
+
+    //local storage is used to override OS theme settings
+    if (localStorage.getItem("theme")) {
+        if (localStorage.getItem("theme") === "dark") {
+            theme = "dark";
+        }
+    } else if (!window.matchMedia) {
+        //matchMedia method not supported
+        return false;
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        //OS theme setting detected as dark
+        theme = "dark";
+    }
+
+    setTheme(theme);
+}
+
+function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+}
+
+detectColorScheme();
