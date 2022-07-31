862600037167736a4f586399d39016ba3498d113
Harieo <harieo@hotmail.com>
2022-07-31 12:40:12 +0100

    Added commenting to Copy.js for nosey people

--- scripts/Copy.js
+++ scripts/Copy.js
@@ -3,9 +3,9 @@ const copied = "Copied!";
 function copyTag() {
     const tagElement = document.getElementById("discord-tag");
     const contents = tagElement.textContent;
-    if (contents !== copied) {
-        navigator.clipboard.writeText(contents).catch(reason => console.error(reason));
-        tagElement.textContent = copied;
-        setTimeout(() => tagElement.textContent = contents, 1000);
+    if (contents !== copied) { // Checks that we're not copying the copied text
+        navigator.clipboard.writeText(contents).catch(reason => console.error(reason)); // Copy text to clipboard
+        tagElement.textContent = copied; // Set text to the copied text
+        setTimeout(() => tagElement.textContent = contents, 1000); // Reset back to what it was originally in 1 second
     }
 }
